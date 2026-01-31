32ee8f3dc997f9e45906ab8a62c95634e9c2bbba
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-01-31 20:34:34 +0530

    update theme configuration to use dark mode and disable theme switch

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -149,11 +149,11 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
