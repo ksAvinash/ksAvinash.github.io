271ec3024b92e0348e170541c8800b8a79166b39
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-02-02 08:48:59 +0530

    fix: update styling of project title in ExternalProjectCard for better readability

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -94,7 +94,7 @@ const ExternalProjectCard = ({
             <div className="w-full">
               <div className="px-4">
                 <div className="text-center w-full">
-                  <h2 className="font-medium text-center opacity-60 mb-2">
+                  <h2 className="text-sm font-semibold text-base-content text-left mb-2">
                     {item.title}
                   </h2>
                   {item.imageUrl && (
